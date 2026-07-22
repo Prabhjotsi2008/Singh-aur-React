1bd303658c1cbcc2cc26e7375976efff4af6186f
Prabhjotsi2008 <prabhjotsi2008@gmail.com>
2026-07-22 20:20:39 +0530

    learnt react flow, structure and conventions

--- 01-vite-react/src/App.jsx
+++ 01-vite-react/src/App.jsx
@@ -1,12 +1,18 @@
+import Chai from "./Chai.jsx" // keep component file names as capital also (CONVENTION)
 
 function App() {
-
   return (
-    <div>
-      <h1>Chai Aur React with Vite | Prabhjot Singh</h1>
-      <p>As always, Chai aur Code and Hitesh Sir never disappoints. The amount of clarity these first two videos gave me is immense. Will keep learning with Chai</p>
-    </div>
-    
+    // <Chai/> // it should be Chai, and not chai // only then it will work
+    // <h1>Hello</h1> // there can not be multiple elements returned
+
+    // Solution
+
+    // these container tags (<> </>) are called fragments used in place of unnecessary divs
+    <> 
+    <Chai/>
+    <h1>Hello</h1>
+    <p>Prabhjot Singh</p>
+    </>
   )
 }
 
---
+++ 01-vite-react/src/Chai.jsx
@@ -0,0 +1,11 @@
+// we can name it chai.js as well  (but .js work for normal react)
+// (for react via vite component .jsx must be preferred)
+
+// Always name component function with UPPERCASED first character
+function Chai(){
+    return(
+        <h3>Chai is ready</h3>
+    )
+}
+
+export default Chai;
